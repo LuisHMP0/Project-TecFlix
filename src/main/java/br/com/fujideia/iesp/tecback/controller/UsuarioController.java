7d69298fdb8f3e76fc7b274b387e2e015b44ec1d
Thiago Felinto <thiagofelintocd@gmail.com>
2024-05-28 21:10:51 -0300

    RF1 - EndPoint AtualizarUser
    Criado o end point Att Usuario, só funciona se o "Private UsuarioRepository" estiver em public

--- src/main/java/br/com/fujideia/iesp/tecback/controller/UsuarioController.java
+++ src/main/java/br/com/fujideia/iesp/tecback/controller/UsuarioController.java
@@ -19,5 +19,19 @@ public class UsuarioController {
     public List<Usuario> listarUsuarios() {
         return usuarioService.listarUsuarios();
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> atualizarUsuario(@PathVariable Long id, @RequestBody Usuario usuario) {
+        // Verificar se o usuário existe
+        if (!usuarioService.usuarioRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        // Atualizar usuário
+        usuario.setId(id); // Certificar que o ID do caminho está usado
+        Usuario usuarioAtualizado = usuarioService.atualizarUsuario(usuario);
+        return ResponseEntity.ok(usuarioAtualizado);
+    }
+
 }
 
